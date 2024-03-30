a072616942c375101c55b886fc0e3be68c1c201f
liucanhui <1251303815@qq.com>
2024-03-30 18:43:47 +0800

    部署主网产生文件提交

--- src/nft_frontend/src/pageShowIdentity/index.jsx
+++ src/nft_frontend/src/pageShowIdentity/index.jsx
@@ -40,15 +40,6 @@ function Page() {
     }, [])
 
     const handleCopyClick = () => {
-        try {
-            if (pageData.ic_account_id) copy(pageData.ic_account_id)
-            void .show('已复制到剪贴板')
-        } catch {
-            void Toast.show({
-                content: '复制失败',
-                icon: 'fail',
-            })
-        }
     }
 
     return (
