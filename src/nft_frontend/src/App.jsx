f316b353ffde1632e4b4e754ea60eb11657b8856
zhanggaofeng <zhanggaofeng@sensorsdata.cn>
2024-04-03 21:53:22 +0800

    修改createActor id

--- src/nft_frontend/src/App.jsx
+++ src/nft_frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const { isAuthenticated, identity } = useAuth();
   return (
     <>
+    <h1>V 1.0.0</h1>
       <header id="header">
         <section id="status" className="toast hidden">
           <span id="content"></span>
